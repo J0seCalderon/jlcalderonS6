5b76a4a37ee5bfa39de5bd7ffe6df398602f2898
Jose Luis Calderon <alfred-95_44@hotmail.com>
2024-05-12 22:33:33 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace jlcalderonS6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Ventanas.vPrincipal();
+        }
+    }
+}
---
+++ Ventanas/vPrincipal.xaml.cs
@@ -0,0 +1,26 @@
+using jlcalderonS6.Tablas;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace jlcalderonS6.Ventanas;
+
+public partial class vPrincipal : ContentPage
+{
+	private const string Url = "http://localhost/appmovil/post.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Estudiante> estud;
+	public vPrincipal()
+	{
+		InitializeComponent();
+		Obtener();
+	}
+
+	public async void Obtener()
+	{
+		var content = await cliente.GetStringAsync(Url);
+		List<Estudiante> mostrarEst = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+		estud = new ObservableCollection<Estudiante>(mostrarEst);
+		listaEstudiantes.ItemsSource = estud;
+	}
+}
