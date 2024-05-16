855817b04965e1f8aa83ad6c89156078505fd87e
Jose Luis Calderon <alfred-95_44@hotmail.com>
2024-05-15 23:02:27 -0500

    Cambio en Post.PHP

--- Ventanas/vActElim.xaml.cs
+++ Ventanas/vActElim.xaml.cs
@@ -1,26 +1,83 @@
 using jlcalderonS6.Tablas;
+using Newtonsoft.Json;
+using System.Net;
+using System;
+using System.Net.Http;
+using System.Text;
+
+
 
 namespace jlcalderonS6.Ventanas;
 
 public partial class vActElim : ContentPage
 {
+    private Estudiante datos;
 
-	public vActElim(Estudiante datos)
+    public vActElim(Estudiante datos)
 	{
 		InitializeComponent();
+        this.datos = datos;
         txtCodigo.Text= datos.codigo.ToString();
         txtNombre.Text = datos.nombre; 
         txtApellido.Text = datos.apellido; 
         txtEdad.Text = datos.edad.ToString();
 	}
 
-    private void bntActualizar_Clicked(object sender, EventArgs e)
+    private async void bntActualizar_Clicked(object sender, EventArgs e)
     {
 
+        try
+        {
+            HttpClient cliente = new HttpClient();
+            var url = "http://localhost/appmovil/post.php";
+            var parametros = new
+            {
+                codigo = txtCodigo.Text,
+                nombre = txtNombre.Text,
+                apellido = txtApellido.Text,
+                edad = txtEdad.Text
+            };
+            var json = JsonConvert.SerializeObject(parametros);
+            var contenido = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var respuesta = await cliente.PutAsync(url, contenido);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Éxito", "Registro actualizado correctamente", "Cerrar");
+                await Navigation.PushAsync(new vPrincipal());
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo actualizar el registro", "Cerrar");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Alerta", ex.Message, "Cerrar");
+        }
     }
 
-    private void bntEliminar_Clicked(object sender, EventArgs e)
+    private async void bntEliminar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            HttpClient cliente = new HttpClient();
+            var url = $"http://localhost/appmovil/post.php?codigo={datos.codigo}";
+            var respuesta = await cliente.DeleteAsync(url);
 
+            if (respuesta.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Éxito", "Registro eliminado correctamente", "Cerrar");
+                await Navigation.PushAsync(new vPrincipal());
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo eliminar el registro", "Cerrar");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Alerta", ex.Message, "Cerrar");
+        }
     }
 }
